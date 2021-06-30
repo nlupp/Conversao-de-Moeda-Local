00bd4f9926272e7b4700f85b61eca9d0b603b59d
nlupp <nlupp@brq.com>
2021-06-30 09:04:11 -0300

    estruturação de ideia inical usando switch case para valor da mmoeda ogrinal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,51 @@
+//variaveis globais 
+var texto = $("#valores").val();
+var seletor1 = $("#moeda1").val();
+var seletor2 = $("#moeda2").val();
+
 $(document).ready(function () {
-    alert("eu to funcionando");
+    form =  $('#formConversao');
+    form.submit(converter);
 });
+
+function converter(){
+    //let valAConv = texto.val();
+
+    // apos verificar as condições imprime num p a resposta.
+    // adicionar variavel $("<p>").text("O valor convertido fica em: " adiconar variavel);
+
+    return false;
+}
+
+// o que fazer primeiro? Pegar o valor adicionado ao input
+function valorInical( texto, seletor1) {
+
+    let moedaInicial;
+
+        switch (seletor1) {
+        case"BRL":
+           
+            break;
+        case"EUA":
+            
+            break;
+        case"EUR":
+           
+            break;
+        case"LIB":
+            
+            break;
+
+            break;
+        case"RMB":
+            
+            break;
+
+        default:
+            console.log('Não sei que tipo de moeda é essa');
+            break;
+    }
+    
+    return moedaInicial;
+}
+
